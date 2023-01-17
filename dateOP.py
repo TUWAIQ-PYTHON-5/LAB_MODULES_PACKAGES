a0bc02649616a5b05520d166e1a925c07963e1f9
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-17 14:41:01 +0300

    lab solv

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+def currentDate():
+    today = date.today()
+    print("Today's date:", today)
+
+print(currentDate())
---
+++ main.py
@@ -0,0 +1,3 @@
+import dateOP 
+
+dateOP.currentDate()
