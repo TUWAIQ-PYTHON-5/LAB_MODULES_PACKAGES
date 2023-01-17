f4e00856f0c57c4ddc7db404e42f15a72c3af6de
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-17 14:56:22 +0300

    solve it

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+# Import date from datetime module
+from datetime import date
+
+# Read the current date from the system
+current_date = date.today()
+# Print the current date without formatting
+print("Today is: ", current_date)
